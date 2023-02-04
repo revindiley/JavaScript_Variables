b6a245b949598b10ea3c4c346885b3e819e0a5a8
Devin Riley <devin.riley144@gmail.com>
2023-02-04 11:40:47 -0600

    finished exercise

--- app.js
+++ app.js
@@ -4,7 +4,25 @@ console.log(
 );
 
 // Exercise 1
-
-// Exercise 2
+const firstName = `Devin`;
+let lastName = `Riley`;
+var age = 24;
 
 // Exercise 3
+let language = "JavaScript"
+let createdYear = 1995;
+let isCaseSensitive = true;
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false; 
+let SelectedSize = "M";
+
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 465;
+let hasRead = true;
